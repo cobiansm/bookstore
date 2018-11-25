411c36f71c5d3a3f04b4c959d1f23a18893b9faf
unknown <sandra_smca@hotmail.com>
2018-11-24 19:27:55 -0600

    ts y paginas

--- bookstore/src/pages/home/home.ts
+++ bookstore/src/pages/home/home.ts
@@ -6,12 +6,23 @@ import { AlertController, NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-books = [];
-book = "";
+
+books = [
+  {titulo: 'El Gran Gatsby', autor: 'F. Scott Fitzgerald', year: '1952'},
+  {titulo: 'For Whom the Bell Tolls', autor: 'Ernest Hemingway', year: '1940'}
+]
 
   constructor(public navCtrl: NavController,
     public alertCtrl: AlertController) {
 
+  }
+
+  clickLibro(b) {
+    this.navCtrl.push(this.libro, {book: b})
+  }
+
+  nuevoLibro() {
+    this.navCtrl.push(this.nuevoLibro, {books:this.books })
   }
 
 
